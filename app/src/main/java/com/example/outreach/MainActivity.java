cb3a1dc44390e587e1e11fc22162461748013b96
Matthew Kaulfers <matthewkaulfers@student.fullsail.edu>
2020-10-10 18:45:56 -0500

    [master]: Removed redundant/unnessasary views for a mor modular approach.

--- app/src/main/java/com/example/outreach/MainActivity.java
+++ app/src/main/java/com/example/outreach/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.outreach;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import java.util.Objects;
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hide the title for the menu.
+        Objects.requireNonNull(getSupportActionBar())
+                .setDisplayShowTitleEnabled(false);
+
+        androidx.appcompat.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorMenuBars)));
     }
 }
