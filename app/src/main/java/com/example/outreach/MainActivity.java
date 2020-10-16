f88dda32d5e92040e2906db9f0062a05dbe7c35b
Matthew Kaulfers <matthewkaulfers@student.fullsail.edu>
2020-10-15 19:35:13 -0500

    [FragmentSetup]: Added SettingsActivity.

--- app/src/main/java/com/example/outreach/MainActivity.java
+++ app/src/main/java/com/example/outreach/MainActivity.java
@@ -1,30 +1,77 @@
 package com.example.outreach;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.example.outreach.fragments.EventViewFragment;
+import com.example.outreach.fragments.SettingsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import java.util.Objects;
-import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     public static BottomNavigationView bottomNavigationView;
+    Menu menu;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.top_menu, menu);
+        this.menu = menu;
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings_action:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.profile_action:
+                break;
+        }
+
+//        ActionBar actionBar = getSupportActionBar();
+//
+//        if (item.getItemId() == R.id.settings_action) {
+//            FrameLayout placeHolderView = findViewById(R.id.settingsFrame);
+//            placeHolderView.setVisibility(View.VISIBLE);
+//
+//            BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+//            bottomNavigationView.setVisibility(View.INVISIBLE);
+//
+//            if (actionBar != null) {
+//                actionBar.setDisplayHomeAsUpEnabled(true);
+//                MenuItem settings = menu.findItem(R.id.settings_action);
+//                MenuItem profile = menu.findItem(R.id.profile_action);
+//
+//                settings.setVisible(false);
+//                profile.setVisible(false);
+//            }
+//
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .replace(R.id.settingsFrame, SettingsFragment.newInstance())
+//                    .commit();
+//        } else {
+//
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .remove(getSupportFragmentManager().findFragmentById(R.id.settingsFrame))
+//                    .commit();
+//        }
+//
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
---
+++ app/src/main/java/com/example/outreach/fragments/SettingsFragment.java
@@ -0,0 +1,22 @@
+package com.example.outreach.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.outreach.R;
+
+public class SettingsFragment extends Fragment {
+
+    public static SettingsFragment newInstance() {
+        return new SettingsFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.settings_view, container, false);
+    }
+}
