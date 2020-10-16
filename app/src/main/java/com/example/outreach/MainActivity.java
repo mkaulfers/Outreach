a9640b16c0f9782f9814d12e9aa5c998fd3f3a7d
Matthew Kaulfers <matthewkaulfers@student.fullsail.edu>
2020-10-15 19:54:21 -0500

    [Fragment Setup]: Added profile activity.

--- app/src/main/java/com/example/outreach/MainActivity.java
+++ app/src/main/java/com/example/outreach/MainActivity.java
@@ -1,31 +1,24 @@
 package com.example.outreach;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.FrameLayout;
-
 import com.example.outreach.fragments.EventViewFragment;
-import com.example.outreach.fragments.SettingsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     public static BottomNavigationView bottomNavigationView;
-    Menu menu;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.top_menu, menu);
-        this.menu = menu;
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -33,44 +26,14 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.settings_action:
-                Intent intent = new Intent(this, SettingsActivity.class);
-                startActivity(intent);
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
                 break;
             case R.id.profile_action:
+                Intent profileIntent = new Intent(this, ProfileActivity.class);
+                startActivity(profileIntent);
                 break;
         }
-
-//        ActionBar actionBar = getSupportActionBar();
-//
-//        if (item.getItemId() == R.id.settings_action) {
-//            FrameLayout placeHolderView = findViewById(R.id.settingsFrame);
-//            placeHolderView.setVisibility(View.VISIBLE);
-//
-//            BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-//            bottomNavigationView.setVisibility(View.INVISIBLE);
-//
-//            if (actionBar != null) {
-//                actionBar.setDisplayHomeAsUpEnabled(true);
-//                MenuItem settings = menu.findItem(R.id.settings_action);
-//                MenuItem profile = menu.findItem(R.id.profile_action);
-//
-//                settings.setVisible(false);
-//                profile.setVisible(false);
-//            }
-//
-//            getSupportFragmentManager()
-//                    .beginTransaction()
-//                    .replace(R.id.settingsFrame, SettingsFragment.newInstance())
-//                    .commit();
-//        } else {
-//
-//            getSupportFragmentManager()
-//                    .beginTransaction()
-//                    .remove(getSupportFragmentManager().findFragmentById(R.id.settingsFrame))
-//                    .commit();
-//        }
-//
-
         return super.onOptionsItemSelected(item);
     }
 
