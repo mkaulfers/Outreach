76b9cd8197147c0f87553e871ce85cd950771269
Matthew Kaulfers <matthewkaulfers@student.fullsail.edu>
2020-10-15 21:13:49 -0500

    [FragmentSetup]: Added new APIDatahandler to utilities.

---
+++ app/src/main/java/com/example/outreach/utilities/APIDataHandler.java
@@ -0,0 +1,6 @@
+package com.example.outreach.utilities;
+
+public class APIDataHandler {
+    ///FINGERPRINT SHA1 - BC:84:86:68:BE:1F:4B:15:AF:A0:A1:E1:1E:5B:B9:BD:AD:7C:B7:51
+    private static final String KEY = "AIzaSyCyHqIf2cFSsVkJreKp0vjCIeHcaj35rVg";
+}
